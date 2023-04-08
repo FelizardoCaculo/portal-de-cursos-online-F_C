98f947704229467185e0bd3bd17075f5423a63a8
FelizardoCaculo <felizardo1987@gmail.com>
2023-04-08 19:24:17 +0100

    Fixing layout issues

--- src/script.js
+++ src/script.js
@@ -1,11 +1,9 @@
-const menu = document.querySelector('#menu-bars');
-const navbar = document.querySelector('.navbar');
-
+let menu = document.querySelector('#menu-bars');
+let navbar = document.querySelector('.navbar');
 menu.onclick = () => {
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
 }
-
 window.onscroll = () => {
     menu.classList.remove('fa-times');
     navbar.classList.toggle('active');
