89d8891b3159665a8fb853e7df4007712559dfff
FelizardoCaculo <felizardo1987@gmail.com>
2023-04-10 21:18:38 +0100

    configuring inline-css for this application

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const router = express.Router();
 const cache = require('memory-cache');
-
 router.get('/', (_req, res) => {
     res.sendFile(__dirname + '/index.html');
 })
